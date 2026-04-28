965e7fb3cfb840f157f761cb11673e11f3e7067d
EduMoonz <cedusantos7@outlook.com>
2026-04-28 17:15:56 -0300

    Tela de Esqueci a senha

--- PIM_2026/PIM_2026/Controllers/ContaController.cs
+++ PIM_2026/PIM_2026/Controllers/ContaController.cs
@@ -13,8 +13,26 @@ public class ContaController : Controller
         return View();
     }
 
+    // GET → abre a tela
+    [HttpGet]
     public IActionResult Esqueciasenha()
     {
+        return View();
+    }
+
+    // POST → recebe o formulário
+    [HttpPost]
+    public IActionResult Esqueciasenha(string email, string novaSenha, string confirmarSenha)
+    {
+        // Verifica se as senhas são diferentes
+        if (novaSenha != confirmarSenha)
+        {
+            ViewBag.Erro = "As senhas não coincidem.";
+            return View();
+        }
+
+        // Aqui depois você pode atualizar no banco
+        ViewBag.Sucesso = "Senha redefinida com sucesso!";
         return View();
     }
 }
